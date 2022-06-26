94303112e8a14fa03dcf8ef4da19f88e574c4adf
Rohit Kushwaha <65504671+rokush@users.noreply.github.com>
2022-06-26 21:22:18 +0530

    Add files via upload

---
+++ actions/actions.py
@@ -0,0 +1,32 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_clubs"
+
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        club = tracker.get_slot('club')
+        url = "https://abes.ac.in"
+
+        if club == "Technical":
+            url = "https://abes.ac.in"
+
+        dispatcher.utter_message({"text": url, "image": "https://i.imgur.com/nGF1K8f.jpg"})
+        return []
